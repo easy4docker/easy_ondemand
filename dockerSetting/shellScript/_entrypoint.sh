fd4c913ae0f320ac078486b8a7e3c36e5aa46bc2
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-03-03 17:07:55 -0800

    Update _entrypoint.sh

--- dockerSetting/shellScript/_entrypoint.sh
+++ dockerSetting/shellScript/_entrypoint.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 cd /var/_localApp
-# echo $mainIP >> /var/_shareFolder/mainIP.txt
 node test.js >> /var/_shareFolder/easy_ondemand.txt
 echo "true" >> /var/_shareFolder/ondemand_finished.data
-# upload all file within /var/_shareFolder
-#if [ $onDemandCallbackHost = "localhost" ]; then
-#  goalHost=${mainIP}:10000
-#else
-#  goalHost=${onDemandCallbackHost}
-#fi
-#echo "callbackHost: "$onDemandCallbackHost > /var/_shareFolder/config.txt
-#echo "superPowerServer: "$superPowerServer >> /var/_shareFolder/config.txt
-#echo "goalHost: "$goalHost >> /var/_shareFolder/config.txt
-
-#curl -F "objPath=/var/_localAppData/sitesShareFolder/${superPowerServer}/gridReturn_$(date +%s)" $(find /var/_shareFolder/ -name '*' -type f -exec echo -n -F "file=@{} " \;) ${goalHost}/upload > /var/_shareFolder/SB.txt
-# rm -fr /var/_shareFolder/
 echo '{"code" : "removeMe"}' > /var/_localAppData/commCron/removeMe.json
-#cp /var/shellScript/removeMe.json /var/_localAppData/commCron/
