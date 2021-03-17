84a063d25a0716c7a2791dd09c06c4d2bdf772e4
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-03-17 16:56:05 -0700

    Update test.js

--- app/test.js
+++ app/test.js
@@ -1,6 +1,13 @@
+const process = require('process');
 console.log('runing start -> ' + new Date());
+
+setInterval(()=> {
+  console.log('.. ' + new Date());
+}, 1000)
+
 setTimeout(
   ()=> {
     console.log('runing end -> ' + new Date());
+    process.exit()
   }, 60000
 );
