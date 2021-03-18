580ee8a70b4bb0c6726a690f656e1a5f2fdf124b
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-03-18 15:58:36 -0700

    Update test.js

--- app/test.js
+++ app/test.js
@@ -1,6 +1,17 @@
+/*
 const fs = require('fs');
+const request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+*/
 fs.readFile('/var/_sharedFolder/input/inputData.data', 'utf-8', (err, data) => {
-  console.log(data);
+  const list = data.split("\n");
+  for (let i =0; i < list.length; i++) {
+    console.log(list[i]);
+  }
 });
 /*
 fs.readDir
