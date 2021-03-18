6b7229b3e3e7038631495d485426714a8b0267d2
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-03-17 17:11:01 -0700

    Update _entrypoint.sh

--- dockerSetting/shellScript/_entrypoint.sh
+++ dockerSetting/shellScript/_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var/_localApp
-node test.js >> /var/_shareFolder/output/easy_ondemand.txt
+node test.js >> /var/_shareFolder/output/console.log.txt
 echo "true" >> /var/_shareFolder/ondemand_finished.data
 echo '{"code" : "removeMe"}' > /var/_localAppData/commCron/removeMe.json
