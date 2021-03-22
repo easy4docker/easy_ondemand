859329ebe2dc0bd449eccf54d1e2486cd1e5e2ae
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-03-21 21:51:27 -0700

    Update test.js

--- app/test.js
+++ app/test.js
@@ -11,23 +11,30 @@ request('http://www.google.com', function (error, response, body) {
 const fn = '/var/_sharedFolder/input/inputData.data';
 // const fn = __dirname + '/data.data'
 */
-const fs = require('fs');
-fs.readFile(fn, 'utf-8', (err, data) => {
-  console.log('start');
+try {
+  
+    const fs = require('fs');
+    fs.readFile(fn, 'utf-8', (err, data) => {
+      console.log('start');
 
-  try {
-    console.log('start 2');
-    const list = data.split(JSON.stringify('\n'));
-    console.log('start');
-    for (let i =0; i < list.length; i++) {
-      console.log(i + ':' + list[i]);
-    }
-    console.log('end');
-  } catch (e) {
-    console.log(e.message);
+      try {
+        console.log('start 2');
+        const list = data.split(JSON.stringify('\n'));
+        console.log('start');
+        for (let i =0; i < list.length; i++) {
+          console.log(i + ':' + list[i]);
+        }
+        console.log('end');
+      } catch (e) {
+        console.log(e.message);
+      }
+      console.log('end2');
+    }); 
+  
+} catch (eRoot) {
+    console.log(eRoot.message);
   }
-  console.log('end2');
-});
+
 
 /*
 const process = require('process');
